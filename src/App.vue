f0662083b573c8a7ebe1cc96a9f684c4cdc3edde
Shahnawaz-Muhammad <shahnawazbaltee55@gmail.com>
2023-11-10 17:46:07 +0500

    tasks,button components added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,43 @@
-<script setup>
-</script>
-
 <template>
-  <router-view/>
+  <h1>Hellow World</h1>
+  <Header title="Header component"/>
+  <Tasks :tasks="tasks"/>
 </template>
+
+<script>
+import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
+
+export default {
+  name: 'App',
+  components: { Header, Tasks },
+  data() {
+    return {
+      tasks :[]
+    }
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'March 21 at 2:20PM',
+        reminder: true
+      },
+      {
+        id: 2,
+        text: 'Meeting with HOD',
+        day: 'March 21 at 5:30PM',
+        reminder: true
+      },
+      {
+        id: 3,
+        text: 'Shopping',
+        day: 'March 23 at 6:30PM',
+        reminder: true
+      },
+    ]
+  }
+}
+</script>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <h2 class="w-full bg-orange-500 p-3">{{title}}</h2>
+        <Button text="Add Task" color="#85929E"/>
+    </header>
+</template>
+
+<script>
+import Button from '../components/Button.vue'
+export default {
+    name: 'Header',
+    props: {
+        title : String
+    },
+    components: {
+        Button
+    }
+
+}
+</script>
