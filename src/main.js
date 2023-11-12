9d19dcbf24904a0a43a7deb1b9f976ef8b8a6c99
Muhammad Shahnawaz <shahnawazbaltee55@gmail.com>
2023-11-12 23:01:25 +0500

    dashboard charts added

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import './assets/main.css'
-import './index.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
-
-import router from './routing'
+import './assets/main.css'
+import './index.css'
+import router from './router'
 
 const app = createApp(App)
 app.use(router)
