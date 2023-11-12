e6738a33d6b6542ab92451a8a73759758f6a0498
Muhammad Shahnawaz <shahnawazbaltee55@gmail.com>
2023-11-13 00:17:35 +0500

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/vue-dashboard",
+  base: "/vue-dashboard",
   plugins: [vue()],
   resolve: {
     alias: {
